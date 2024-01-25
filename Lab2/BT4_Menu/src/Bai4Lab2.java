e885c5869936cbec06ad28f455c23b392af9cf29
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-01-25 20:59:08 +0700

    update

--- Lab2/BT4_Menu/src/Bai4Lab2.java
+++ Lab2/BT4_Menu/src/Bai4Lab2.java
@@ -17,7 +17,7 @@ public class Bai4Lab2 {
 			chon = scanner.nextInt();
 			
 			switch (chon) {
-			case 1:
+			case 1: //Giải PT bậc 1
 				System.out.print("Nhập hệ số a: ");
 				double a = scanner.nextDouble();
 				System.out.print("Nhập hệ số b: ");
@@ -33,17 +33,50 @@ public class Bai4Lab2 {
 					System.out.printf("PT có nghiệm x = %.1f", x);
 				}
                 break;
-            case 2:
-                // Gọi hàm giải phương trình bậc hai
+            case 2: //Giải PT bậc 2
+            	System.out.println("Nhập hệ số a: ");
+        		double a1 = scanner.nextDouble();
+        		System.out.println("Nhập hệ số b: ");
+        		double b1 = scanner.nextDouble();
+        		System.out.println("Nhập hệ số c: ");
+        		double c1 = scanner.nextDouble();
+        		
+        		if(a1==0) {
+        			if(b1==0)
+        				System.out.println("PT có vô số nghiệm!");
+        			else
+        				System.out.println("PT có vô nghiệm!");
+        		}
+        		else {
+        			double delta = b1*b1 - 4*a1*c1;
+        			double x1 = (-b1 + Math.sqrt(delta)) / (2 * a1), x2 = (-b1 - Math.sqrt(delta)) / (2 * a1);
+        			double x_kep = -b1/(2*a1);
+        			
+        			if(delta<0)
+        				System.out.println("PT vô nghiệm!");
+        			else if(delta == 0)
+        				System.out.printf("PT có nghiệm kép: x1 = x2 = %.2f", x_kep);
+        			else
+        				System.out.printf("PT có 2 nghiệm: x1 = %.2f, x2 = %.2f",x1,x2);
+        		}
                 break;
-            case 3:
-                // Gọi hàm tính tiền điện theo phương pháp lũy tiến
+            case 3: //Tính tiền điện
+            	System.out.print("Nhập vào số kWh điện đã sử dụng: ");
+        		double kWh = scanner.nextDouble();
+        		double tien = 0;
+        		
+        		if(kWh <= 50)
+        			tien = kWh*1000;
+        		else
+        			tien = 50*1000 + (kWh - 50)*1200;
+        		
+        		System.out.printf("Số tiền điện cần thanh toán là: %.2f VNĐ\n", tien);
                 break;
             case 4:
-                System.out.print("Bạn đã chọn Exit!");
+                System.out.println("Bạn đã chọn Exit!");
                 break;
             default:
-                System.out.print("Lựa chọn không có trong Menu!");
+                System.out.println("Lựa chọn không có trong Menu!");
                 break;
 			}
 		}while(chon != 4);
