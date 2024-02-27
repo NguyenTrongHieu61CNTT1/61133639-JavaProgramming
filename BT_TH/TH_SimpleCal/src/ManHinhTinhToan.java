d3f3100bddfddb008b9bfff9422f09aab3cf2be5
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-02-27 09:19:43 +0700

    BT_TH
    update

--- BT_TH/TH_SimpleCal/src/ManHinhTinhToan.java
+++ BT_TH/TH_SimpleCal/src/ManHinhTinhToan.java
@@ -9,32 +9,14 @@ public class ManHinhTinhToan extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ManHinhTinhToan frame = new ManHinhTinhToan();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public ManHinhTinhToan() {
+		setTitle("Máy Tính Đơn Giản");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 628, 488);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 	}
 
 }
--- BT_TH/TH_SimpleCal/src/TinhToan.java
+++ BT_TH/TH_SimpleCal/src/TinhToan.java
@@ -3,7 +3,7 @@ public class TinhToan {
 
 	public static void main(String[] args) {
 		// Màn hình GUI
-		
+		new ManHinhTinhToan().setVisible(true);
 	}
 
 }
