290f7f60593283b37f2ae5b4cdc8670b98966133
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-02-28 10:16:04 +0700

    TH_QLBH
    Done

--- BT_TH/TH_QLBH/src/LoginFrame.java
+++ BT_TH/TH_QLBH/src/LoginFrame.java
@@ -4,12 +4,21 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class LoginFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField txtUser;
+	private JPasswordField txtPass;
 
 	
 	public LoginFrame() {
@@ -31,5 +40,44 @@ public class LoginFrame extends JFrame {
 		lblMtKhu.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblMtKhu.setBounds(10, 96, 153, 32);
 		contentPane.add(lblMtKhu);
+		
+		txtUser = new JTextField();
+		txtUser.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		txtUser.setBounds(165, 43, 171, 30);
+		contentPane.add(txtUser);
+		txtUser.setColumns(10);
+		
+		txtPass = new JPasswordField();
+		txtPass.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		txtPass.setBounds(110, 96, 226, 28);
+		contentPane.add(txtPass);
+		
+		JButton btnLogin = new JButton("Đăng Nhập");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//Xử lý đăng nhập
+				XuLyDangNhap();
+			}
+		});
+		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnLogin.setBounds(368, 45, 114, 30);
+		contentPane.add(btnLogin);
+	}
+	
+	void XuLyDangNhap() {
+		//Lấy user và pass:
+		String str_user = txtUser.getText();
+		String str_pass = txtPass.getText();
+		
+		//Xử lý:
+		if(str_user.equals("63CNTT") && str_pass.equals("123")) {
+			HomeFrame homePage = new HomeFrame();
+			homePage.setVisible(true);
+			this.setVisible(false); //Ẩn form login
+		}
+		else {
+			JOptionPane hopThoai = new JOptionPane();
+			hopThoai.showMessageDialog(this, "Đăng nhập thất bại");
+		}
 	}
 }
