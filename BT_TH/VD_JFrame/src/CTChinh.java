691eedafb2195ef774aeb267e0d5edb786359a83
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-02-28 09:35:04 +0700

    VDJFrame
    Tạo khung

---
+++ BT_TH/VD_JFrame/src/CTChinh.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class CTChinh {
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		f.setTitle("VD JFrame");
+		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		f.setSize(300, 200); //Kích thước
+		f.show();
+	}
+}
